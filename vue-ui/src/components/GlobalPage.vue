9180c6ee0971c7f0c1514e7206aa94e3b4cddeae
SHIDi233 <1985136419@qq.com>
2023-12-21 00:56:21 +0800

    1

--- vue-ui/src/components/GlobalPage.vue
+++
@@ -1,9 +0,0 @@
-<script>
-    // const ip = 'http://123.57.181.79:8888/'; //名称
-    const ip = 'http://124.70.194.61:8888/'; //名称
-    // const ip = 'http://127.0.0.1:8888/'
-
-    export default {
-        ip
-    }
-</script>
--- vue-ui/src/components/HePage.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div style="display:flex;justify-content:center;align-items:center;">
-        <div style=" text-align: center;margin:auto;">👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋</div>
-    </div>
-    
-</template>
